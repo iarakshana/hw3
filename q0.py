6aa5529e6d41ba88856e7f783dc1030898713276
aycrazy <andrewyaspan@gmail.com>
2017-10-27 10:17:42 -0500

    TA comments

--- q0.py
+++ q0.py
@@ -6,8 +6,8 @@ vec = get_data("salaries.csv")
 #defined a function that can sort within the list of the list on specifically the salaries column by making that column specifically
 #a list
 def salaries_dict(vec):
-        dict_sal = {}
-        for i in range(1,len(vec)):
+    dict_sal = {}
+    for i in range(1,len(vec)):
         l = vec[i]
         salary = l[6]
         #print(salary)
@@ -17,8 +17,13 @@ def salaries_dict(vec):
         #sorting the dict_sal from reverse gives us the largest to smallest
     return sorted(dict_sal.items(), reverse=True)
 
-salaries_dict(vec)
+print(salaries_dict(vec))
 
 sorted(vec, key=lambda x: (x[6]), reverse=True) #also can use list comprehension with labda to sort
 
+
 solution = "EVANS" # LAST NAME IN CAPS.
+
+'''Grader Comments:
+You somehow pushed this without proper indentation.  I fixed it and the result of your function does not
+give the answer.'''
